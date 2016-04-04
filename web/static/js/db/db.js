d3ccf3433f424f82d026121244814efcab19e5c6
Juan Francisco Del Solar <juandelsolar@gmail.com>
2016-04-04 03:38:48 -0300

    archivo de servidores y sincronización

--- web/static/js/db/db.js
+++ web/static/js/db/db.js
@@ -1,9 +1,36 @@
 import PouchDB from 'pouchdb';
 import PouchFind from 'pouchdb-find';
 PouchDB.plugin(PouchFind);
+import { couchServers } from './servers';
 
-const db = new PouchDB('http://toth.cl:5984/schedule');
+const dbName = 'schedule';
+const db = new PouchDB(dbName);
+const dbSync = { 'name': 'None', 'dbIndex': 0 };
+
 export default class DB {
+
+    static setActiveServer() {
+        var url = couchServers[dbSync.dbIndex].url + '/' + dbName;
+        dbSync.name = couchServers[dbSync.dbIndex].name;
+        db.sync(url, {
+            live: true,
+            retry: true
+        }).on('change', function (info) {
+            console.log('change ', dbSync.name ,info);
+        }).on('paused', function () {
+            console.log('paused ', dbSync.name);
+        }).on('active', function () {
+            console.log('active ', dbSync.name);
+        }).on('denied', function (info) {
+            console.log('denied ', dbSync.name, info);
+        }).on('complete', function (info) {
+            console.log('complete ', dbSync.name, info);
+        }).on('error', function (err) {
+            console.log('error ', dbSync.name, err);
+            dbSync.dbIndex += 1;
+            this.setActiveServer();
+        });
+    }
 
     /**
      * { function_description }
@@ -41,7 +68,7 @@ export default class DB {
                 });
         });
     }
-    
+
     /**
      * Promise.all
      *
@@ -50,17 +77,18 @@ export default class DB {
      * @return     {Promise}  return Object with config
      */
     static getConfigSchedule(_id) {
+        this.setActiveServer();
         return new Promise((resolve, reject) => {
             let promises = [this.getDefaultOptions(), this.getRoom(_id)];
             Promise.all(promises)
-                .then(responses => { 
+                .then(responses => {
                     let json = {
                         defaultOptions: responses[0].options,
                         roomOptions: responses[1]
                     };
                     resolve(json);
                 })
-                .catch(err => { 
+                .catch(err => {
                     reject(err);
                 });
         });
---
+++ web/static/js/db/servers.js
@@ -0,0 +1,10 @@
+export const couchServers = [
+    {
+        'name': 'cloudant',
+        'url': 'https://toth.hcloudant.com'
+    },
+    {
+        'name': 'toth',
+        'url': 'http://toth.cl:5984'
+    }
+]
