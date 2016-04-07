c0da357fc8cca4986f142fdd3014a9f25e4335b2
Simplicio Carcamo <siarcarse@gmail.com>
2016-04-07 18:03:21 -0300

    0.0.0-dev

--- web/static/js/db/db.verify.js
+++ web/static/js/db/db.verify.js
@@ -1,76 +1,45 @@
-import PouchDB from 'pouchdb';
-//import { couchServers } from './servers';
-//import https from 'https';
 import http from 'http';
-//import SyncPromise from 'sync-promise';
-const couchServers = [{
-    'name': 'cloudant',
-    'url': 'https://toth.cloudant.com',
-    'auth': {
-        'username': 'toth',
-        'password': 'Mipapajuandijosisualma15'
-    }
-}, {
-    'name': 'toth',
-    'url': 'http://toth.cl:5984'
-}];
-const dbName = 'schedule';
+import https from 'https';
 
-export function Verify() {
+export function Verify(couchServers) {
     return new Promise((resolve, reject) => {
-        validateUrl()
-            .then((promises) => {
-                Promise.all(promises)
-                    .then((responses) => {
-                        console.log('primer responses', responses);
-                        resolve(responses[0]);
-                    })
-                    .catch((err) => {
-                        console.log('error');
-                        reject(err);
-                    });
-            });
-    });
-}
-
-function urlStatus() {
-    return new Promise((resolve) => {
-        const dbPomises = [];
-        let responses = [];
-        let completedRequests = 0;
-        for (var i = 0; i < couchServers.length; i++) {
-            const server = couchServers[i];
-            http.get(server.url, (response) => {
-                if (response.statusCode === 200) {
-                    responses.push(response.body);
-                    completedRequests++;
-                    let url = `${server.url}/${dbName}`;
-                    let db = new PouchDB(url, server.auth);
-                    if (server.name === 'cloudant') {
-                        dbPomises.unshift(db.info());
-                    } else {
-                        dbPomises.push(db.info());
-                    }
-                    if (completedRequests === couchServers.length) {
-                        resolve(dbPomises);
-                    }
+        urlStatus(couchServers)
+            .then((server) => {
+                if (typeof server[0] === 'object') {
+                    resolve(server[0]);
                 }
-            }).on('error', (e) => {
-                console.log(e);
-            });
-        }
-    });
-}
-
-function validateUrl() {
-    return new Promise((resolve, reject) => {
-        urlStatus()
-            .then((responses) => {
-                console.log('otro responses', responses);
-                resolve(responses);
             })
             .catch((err) => {
                 reject(err);
             });
+
+    });
+}
+
+function urlStatus(couchServers) {
+    return new Promise((resolve, reject) => {
+        let completedRequests = 0;
+        const serversOnline = [];
+        for (var i = 0; i < couchServers.length; i++) {
+            const server = couchServers[i];
+            let serverQuery = (server.ssl) ? https : http;
+            serverQuery.get(server.url, (response) => {
+                completedRequests++;
+                if (server.name === 'cloudant' && response.statusCode === 200) {
+                    server.status = response.statusCode;
+                    serversOnline.unshift(server);
+                } else if (response.statusCode === 200) {
+                    server.status = response.statusCode;
+                    serversOnline.push(server);
+                }
+                if (completedRequests === couchServers.length) {
+                    if (serversOnline.length > 0) {
+                        resolve(serversOnline);
+                    } else {
+                        reject('ALL SERVERS OFFLINE');
+                    }
+                }
+            });
+        }
     });
 }
